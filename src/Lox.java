c3d8470dd17ab1aeb4544c7b6fc3dfe7264328b9
AbhayPratapSingh1 <abhaypratapsingh481833@gmail.com>
2026-04-30 15:26:23 +0530

    adds basic structure

---
+++ src/Lox.java
@@ -0,0 +1,11 @@
+public class Lox {
+    static void main(String[] args) {
+        if (args.length == 0){
+            System.out.println("Done");
+            System.exit(0);
+        }
+        System.out.println("Invalid User");
+        System.exit(1);
+
+    }
+}
